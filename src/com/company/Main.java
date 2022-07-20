6947e3f1d6ab7168cd0a8a934fcc48440ad6c338
Be-Asia <93811176+amerigirl@users.noreply.github.com>
2022-07-19 19:35:45 -0500

    made my first string buffer for bread :-)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
-        System.out.println("\n\nWelcome to Be-Asia's 'Build a Sandwich to Remember' App-- I hope you're hungry!"); part of the app?
+        System.out.println("\n\nWelcome to Be-Asia's 'Build a Sandwich to Remember' App-- I hope you're hungry!");
 
         //can we add a delay here of just a couple of seconds?
 
         System.out.println("Ok, let's get this yummy party started.");
+
         Bread sampleBread = new Bread("rye", "wholeWheat", "white", "honeyWheat", "focacia", "sourDough");
         //we need a small break here--2-3 seconds
         sampleBread.showBreadMenu();
+
 
         Cheese sampleCheese = new Cheese("americanCheddar", "provlone", "swiss", "smokedGouda", "mozzarella", "honeyGoatCheese");
         //we need a small break here--2-3 seconds
         sampleCheese.showCheeseMenu();
 
-        Condiments sampleRandom = new Condiments("mayo", "mustard","rance","hotSauce",  "cranberrySauce");
+
+        Condiments sampleCondiments = new Condiments("mayo", "mustard","rance","hotSauce",  "cranberrySauce");
         //we need a small break here--2-3 seconds
-        sampleRandom.showCondimentsMenu();
+        sampleCondiments.showCondimentsMenu();
+
 
         Meat sampleMeat = new Meat("veggie meat", "salami", "turkey", "ham", "pastrami", "prosciutto", "roast beef");
         //we need a small break here--2-3 seconds
@@ -31,8 +37,27 @@ public class Main {
         //we need a small break here--2-3 seconds
         sampleVeggie.showVeggiesMenu();
 
-        System.out.println("Ok, you've made all of your choices, now, let's put this sandwich together.  Here's your ingredient list: ");
+
+        System.out.println("\n\nOk, you've made all of your choices, now, let's put this sandwich together.  Here's your ingredient list: ");
+
+        StringBuffer breadString = new StringBuffer();
+            for (Object s : sampleBread.getFinalBreadChoice()) {
+                breadString.append(s);
+                String finalBreadString = breadString.toString();
+                System.out.println("\n\nBread  =  " + finalBreadString);
+        }
+
+        System.out.println(sampleBread.getFinalBreadChoice());
+        System.out.println("Cheese =  " + sampleCheese.getFinalCheeseChoice());
+        System.out.println("Condiments =  " + sampleCondiments.getFinalCondimentChoice());
+        System.out.println("Meat =  " + sampleMeat.getFinalMeatChoices());
+        System.out.println("Veggies =  " + (sampleVeggie.getFinalVeggieChoices()));
+
     }
+
+
 }
+//take an array List and make it print as a string
+
 
 //we want to be able to call either a sandwich method that builds the sandwich with the user's help, or a main menu that lets the user cycle through the parts of making a sandwich
